6e42d36f548aff487a1446fbc129e695a78abd7f
Uralbek Yergenov <oralbekergenov@gmail.com>
2023-02-25 12:56:50 +0600

    Создание модели GuestBook

--- src/guestbook/models.py
+++ src/guestbook/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from django.utils.translation import gettext_lazy as _
+
+
+class GuestBook(models.Model):
+    class Status(models.TextChoices):
+        ACTIVE = 'active', _('Активно')
+        BLOCKED = 'blocked', _('Заблокировано')
+
+    author = models.CharField(max_length=256, null=False, blank=False, verbose_name='Имя автора')
+    email = models.EmailField(max_length=1024, null=False, blank=False, verbose_name='Почта автора')
+    description = models.TextField(max_length=4096, null=False, blank=False, verbose_name='Текст')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время редактирования')
+    status = models.CharField(
+        max_length=7,
+        choices=Status.choices,
+        default=Status.ACTIVE,
+    )
+    
