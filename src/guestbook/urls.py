1be5d8c5bcfe46aa1ba83c1cafd072e099dc6c5e
Uralbek Yergenov <oralbekergenov@gmail.com>
2023-02-25 15:55:13 +0600

    Создать страницу для добавления записи

--- src/guestbook/urls.py
+++ src/guestbook/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path('', index, name='home'),
     path('records/', index, name='records'),
+    path('records/add', add_record, name='add_record'),
 ]
--- src/guestbook/views.py
+++ src/guestbook/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import GuestBook
+from .forms import RecordForm
 
 
 def index(request):
@@ -8,3 +9,28 @@ def index(request):
     return render(request, 'guestbook/index.html', context={
         'records': records.order_by('-created_at')
     })
+
+
+def add_record(request):
+    match request.method:
+        case 'GET':
+            form = RecordForm()
+            return render(request, 'guestbook/create.html', context={
+                'form': form,
+            })
+
+        case 'POST':
+            form = RecordForm(request.POST)
+            if not form.is_valid():
+                return render(request, 'guestbook/create.html', context={
+                    'form': form,
+                })
+
+            # SUCCESS
+            GuestBook.objects.create(
+                author=form.cleaned_data.get('author'),
+                email=form.cleaned_data.get('email'),
+                description=form.cleaned_data.get('description')
+            )
+
+            return redirect('home')
